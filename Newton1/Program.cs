86a418c05d07486b430426618117a17194eb3a3a
Adrian Sims <adriansims62781@gmail.com>
2021-05-28 13:39:13 +0200

    Add project files.

---
+++ Newton1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Newton1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+
+                var json = @"{ CPU: 'Intel' }";
+                var r = JObject.Parse(json);
+                Console.WriteLine(r);
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
